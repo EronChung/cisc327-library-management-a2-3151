2f2ddf6b8f79e14c15437ecaac0635054e3280c5
EronChung <22gj23@queensu.ca>
2025-10-13 21:36:54 -0400

    tests\__init__.py : Check if library.db exists first before deleting

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,7 +2,8 @@ import os
 from database import *
 
 # Delete old library.db
-os.remove("library.db")
+if os.path.exists("library.db"):
+    os.remove("library.db")
 
 # Create a new one with sample data
 init_database()
